3cdb595afe499aa785c183e5a32cf09f1438d0cd
ALP <annielee@Annies-MacBook-Pro.local>
2018-09-15 13:51:15 -0700

    finals

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,73 @@
 // =============================
 
+//load page when window is loaded with
+    // score at 0
+    // timer set to 20 sec
+
+//when user clicks start:
+    //timer -- counts down by one sec increments and stops at 0
+//when user selects an answer:
+    //first check if timer is >0
+    //record user input for each question and compare against correct answer
+
+
+    //When the timer reaches zero
+// after time stops:
+    //display correct answer next to incorrect selections
+    
+if($("#question1 option:selected").text() == 5 months) {   
+}
+else{
+    
+}
+)
+    $("#question2 option:selected").text();
+    $("#question3 option:selected").text();
+    $("#question4 option:selected").text();
+    $("#question5 option:selected").text();
+
+    //show score as x/5 correct
+
+
+
 // This code will run as soon as the page loads
 window.onload = function() {
     $("#playBtn").on("click", stopwatch.start);
   };
-  
-  //  Variable that will hold our setInterval that runs the stopwatch
-  var intervalId;
-  
-  // prevents the clock from being sped up unnecessarily
-  var clockRunning = false;
-  
-  // Our stopwatch object
-  var stopwatch = {
-  
-    time: 0,
-  
-    reset: function() {
-  
-      stopwatch.time = 0;
-  
-      // DONE: Change the "display" div to "00:00."
-      $("#ptimer").text("0");
-  
-    },
-    start: function() {
-  
-      // DONE: Use setInterval to start the count here and set the clock to running.
-      if (!clockRunning) {
-        intervalId = setInterval(stopwatch.count, 1000);
-        clockRunning = true;
-      }
-    },
-    stop: function() {
-  
-      // DONE: Use clearInterval to stop the count here and set the clock to not be running.
-      clearInterval(intervalId);
-      clockRunning = false;
-    },
 
-    count: function() {
+$("#playBtn").on("click", stopwatch.count);
   
-      // DONE: increment time by 1, remember we cant use "this" here.
-      stopwatch.time++;
+//   //  Variable that will hold our setInterval that runs the stopwatch
+//   var intervalId;
+  
+//   // prevents the clock from being sped up unnecessarily
+//   var clockRunning = false;
+  
+//   // Our stopwatch object
+//   var stopwatch = {
+  
+//     time: 20},
+  
+//     start: function() {
+  
+//       // DONE: Use setInterval to start the count here and set the clock to running.
+//       if (!clockRunning) {
+//         intervalId = setInterval(stopwatch.count, 1000);
+//         clockRunning = true;
+//       }
+//     },
+//     stop: function() {
+  
+//       // DONE: Use clearInterval to stop the count here and set the clock to not be running.
+//       clearInterval(intervalId);
+//       clockRunning = false;
+//     },
 
-    },
+//     count: function() {
+  
+//       // DONE: increment time by 1, remember we cant use "this" here.
+//       stopwatch.time--;
 
-}
+//     },
+
+
